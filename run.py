abad590aa032f7327ba32888e5a6e5e24079a2c2
Luciano Zanuz <lzanuz@hotmail.com>
2021-06-21 22:55:58 -0300

    Update run.py

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+pip install transformers
+from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
+
 from transformers import BertForMaskedLM  
 model_neuralmind = BertForMaskedLM.from_pretrained('neuralmind/bert-base-portuguese-cased')
 tokenizer_neuralmind = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased', do_lower_case=False)
