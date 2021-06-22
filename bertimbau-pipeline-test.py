43e4e37e43d87b1954149a8eed7c67358e042a1a
Luciano Zanuz <lzanuz@hotmail.com>
2021-06-22 00:33:09 -0300

    Update bertimbau-pipeline-test.py

--- bertimbau-pipeline-test.py
+++ bertimbau-pipeline-test.py
@@ -8,3 +8,6 @@ from transformers import pipeline
 pipe = pipeline('fill-mask', model=model_neuralmind, tokenizer=tokenizer_neuralmind)
 output = pipe('Tinha uma [MASK] no meio do caminho.')
 print(output)
+
+with open("output.txt", "w") as text_file:
+    text_file.write(output)
