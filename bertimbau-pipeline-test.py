cc6ae9ae79b717e1f4cced534a2cb210a77081aa
Luciano Zanuz <lzanuz@hotmail.com>
2021-06-22 00:14:48 -0300

    Update and rename run_test.py to bertimbau-pipeline-test.py

--- bertimbau-pipeline-test.py
+++ bertimbau-pipeline-test.py
@@ -1,8 +1,8 @@
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
 from transformers import BertForMaskedLM  
 
-model_neuralmind = BertForMaskedLM.from_pretrained('neuralmind/bert-base-portuguese-cased', output_loading_info=False)
-tokenizer_neuralmind = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased', do_lower_case=False, output_loading_info=False)
+model_neuralmind = BertForMaskedLM.from_pretrained('neuralmind/bert-base-portuguese-cased')
+tokenizer_neuralmind = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased', do_lower_case=False)
 
 pipe = pipeline('fill-mask', model=model_neuralmind, tokenizer=tokenizer_neuralmind)
 output = pipe('Tinha uma [MASK] no meio do caminho.')
