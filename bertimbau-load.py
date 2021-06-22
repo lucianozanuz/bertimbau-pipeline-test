95af137cd6d32633ec35703c9b473855c2dae1cf
Luciano Zanuz <lzanuz@hotmail.com>
2021-06-22 00:42:20 -0300

    Delete bertimbau-load.py

--- bertimbau-load.py
+++
@@ -1,5 +0,0 @@
-from transformers import AutoModelForTokenClassification, AutoTokenizer
-from transformers import BertForMaskedLM  
-
-model_neuralmind = BertForMaskedLM.from_pretrained('neuralmind/bert-base-portuguese-cased')
-tokenizer_neuralmind = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased', do_lower_case=False)
