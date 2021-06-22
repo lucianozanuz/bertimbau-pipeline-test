a7fba3df29b73d41bcf1089ba1fb04acf09a2141
Luciano Zanuz <lzanuz@hotmail.com>
2021-06-21 23:34:28 -0300

    Update and rename run.py to run_test.py

--- run_test.py
+++ run_test.py
@@ -1,10 +1,9 @@
-#!pip install transformers
 from transformers import pipeline, AutoModelForTokenClassification, AutoTokenizer
-
 from transformers import BertForMaskedLM  
+
 model_neuralmind = BertForMaskedLM.from_pretrained('neuralmind/bert-base-portuguese-cased')
 tokenizer_neuralmind = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased', do_lower_case=False)
 
 pipe = pipeline('fill-mask', model=model_neuralmind, tokenizer=tokenizer_neuralmind)
-pipe('Tinha uma [MASK] no meio do caminho.')
-print(pipe)
+output = pipe('Tinha uma [MASK] no meio do caminho.')
+print(output)
