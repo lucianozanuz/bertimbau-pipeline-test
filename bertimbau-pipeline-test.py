94c49fa7e56478e6a22ad831ef7e60fce0224db3
Luciano Zanuz <lzanuz@hotmail.com>
2021-06-22 00:26:33 -0300

    Update bertimbau-pipeline-test.py

--- bertimbau-pipeline-test.py
+++ bertimbau-pipeline-test.py
@@ -1,3 +1,8 @@
+from transformers import AutoModelForTokenClassification, BertForMaskedLM, AutoTokenizer
+
+model_neuralmind = BertForMaskedLM.from_pretrained('neuralmind/bert-base-portuguese-cased')
+tokenizer_neuralmind = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased', do_lower_case=False)
+
 from transformers import pipeline
 
 pipe = pipeline('fill-mask', model=model_neuralmind, tokenizer=tokenizer_neuralmind)
