e68a891d48f897bc7ce49610ad79e41631504b0a
Luciano Zanuz <lzanuz@hotmail.com>
2021-06-21 22:42:14 -0300

    Create run.py

---
+++ run.py
@@ -0,0 +1,6 @@
+from transformers import BertForMaskedLM  
+model_neuralmind = BertForMaskedLM.from_pretrained('neuralmind/bert-base-portuguese-cased')
+tokenizer_neuralmind = AutoTokenizer.from_pretrained('neuralmind/bert-base-portuguese-cased', do_lower_case=False)
+
+pipe = pipeline('fill-mask', model=model_neuralmind, tokenizer=tokenizer_neuralmind)
+pipe('Tinha uma [MASK] no meio do caminho.')
